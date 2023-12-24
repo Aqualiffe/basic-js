c6f6eaf4ca960784add113089e4a5daef0210f4d
Aqualiffe <evgeniia.er@gmail.com>
2023-12-24 15:29:43 +0300

    feat: add task

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const res = {};
+  for (let i = 0; i < domains.length; i++) {
+    const domain = domains[i].split('.').reverse();
+    for (let j = 0; j < domain.length; j++) {
+      const dns = '.' + domain.slice(0, j + 1).join('.');
+      if (res[dns]) res[dns]++;
+      else res[dns] = 1;
+    }
+  }
+  return res;
 }
 
 module.exports = {
